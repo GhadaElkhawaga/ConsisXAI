caabe54986bb2e07660a17a2f42d3e5c4ceb2cf7
GhadaElkhawaga <48477434+GhadaElkhawaga@users.noreply.github.com>
2021-11-03 16:21:02 +0100

    Add files via upload

---
+++ preprocessing/Indispensable_feats_gen.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.feature_selection import SelectFromModel, SelectKBest
+from sklearn.feature_selection import f_classif, chi2
+from sklearn.preprocessing import MinMaxScaler
+from skrebate import TuRF
+
+
+def compute_impurity(feature, impurity_criterion):
+    """
+    This function calculates impurity of a feature.
+    Supported impurity criteria: 'entropy', 'gini'
+    input: feature (this needs to be a Pandas series)
+    output: feature impurity
+    """
+    probs = feature.value_counts(normalize=True)
+    if impurity_criterion == 'entropy':
+        impurity = -1 * np.sum(np.log2(probs) * probs)
+    elif impurity_criterion == 'gini':
+        impurity = 1 - np.sum(np.square(probs))
+    else:
+        raise ValueError('Unknown impurity criterion')
+
+    return round(impurity, 3)
+
+
+'''a function to compute information gain after splitting based on a certain feature, 
+this function is obtained from 'https://www.featureranking.com/tutorials/machine-learning-tutorials/information-gain-computation/'''
+
+
+def comp_feature_information_gain(df, target, descriptive_feature, split_criterion):
+    """
+    This function calculates information gain for splitting on
+    a particular descriptive feature for a given dataset
+    and a given impurity criteria.
+    Supported split criterion: 'entropy', 'gini'
+    """
+
+    target_entropy = compute_impurity(df[target], split_criterion)
+
+    # we define two lists below:
+    # entropy_list to store the entropy of each partition
+    # weight_list to store the relative number of observations in each partition
+    entropy_list = list()
+    weight_list = list()
+
+    # loop over each level of the descriptive feature
+    # to partition the dataset with respect to that level
+    # and compute the entropy and the weight of the level's partition
+    for level in df[descriptive_feature].unique():
+        df_feature_level = df[df[descriptive_feature] == level]
+        entropy_level = compute_impurity(df_feature_level[target], split_criterion)
+        entropy_list.append(round(entropy_level, 3))
+        weight_level = len(df_feature_level) / len(df)
+        weight_list.append(round(weight_level, 3))
+
+    # compute either the gini split or the entropy split of a feature
+    remaining_features_impurity = np.sum(np.array(entropy_list) * np.array(weight_list))
+    information_gain = target_entropy - remaining_features_impurity
+    return information_gain
+
+
+# a function to compute WOE and IV:
+def calculate_woe_iv(ds, ffeatures, target):
+    IV = []
+    for feat in ffeatures:
+        lst = []
+        for i in range(ds[feat].nunique()):
+            val = list(ds[feat].unique())[i]
+            lst.append({
+                'Value': val,
+                'All': ds[ds[feat] == val].count()[feat],
+                'Good': ds[(ds[feat] == val) & (ds[target] == 0)].count()[feat],
+                'Bad': ds[(ds[feat] == val) & (ds[target] == 1)].count()[feat]
+            })
+
+        df = pd.DataFrame(lst)
+        df['Distr_Good'] = df['Good'] / df['Good'].sum()
+        df['Distr_Bad'] = df['Bad'] / df['Bad'].sum()
+        df['WoE'] = np.log(df['Distr_Good'] / df['Distr_Bad'])
+        df = df.replace({'WoE': {np.inf: 0, -np.inf: 0}})
+        df['IV'] = (df['Distr_Good'] - df['Distr_Bad']) * df['WoE']  # for a single bin
+        IV.append(df['IV'].sum())  # for the whole feature
+
+    return IV
+
+
+# python functions to compute reducts and core of large datasets based on feature selection
+def compute_features_importance(out_dir, ds, X_train, y_train, ffeatures, target_name,
+                                model_xgboost, model_logit, df_discretized, file_name):
+    results_df = pd.DataFrame()
+    mean_threshold = {}
+
+    if ds in ['spect', 'Zoo']:
+        selector_chi_anova = SelectKBest(chi2, k='all').fit(X_train, y_train)
+    else:
+        selector_chi_anova = SelectKBest(f_classif, k='all').fit(X_train, y_train)
+    # Embedded selectors (for logit and xgboost):
+    lasso_selector = SelectFromModel(estimator=model_logit, prefit=True, threshold=-np.inf, max_features=len(ffeatures))
+    tree_selector = SelectFromModel(estimator=model_xgboost, prefit=True, threshold=-np.inf,
+                                    max_features=len(ffeatures))
+
+    # TuRF
+    # make sure to have the 'pct' other than 0.5, if the #of features is odd
+    trelief_selector = TuRF(core_algorithm="ReliefF", n_features_to_select=len(ffeatures),
+                            pct=0.3, verbose=True).fit(X_train.values, y_train.values, ffeatures)
+
+    # Information gain:
+    info_gain_entropy = []
+    info_gain_gini = []
+    # split_criterion = 'entropy'
+    for f in ffeatures:
+        feature_info_gain_entropy = comp_feature_information_gain(df_discretized, target_name, f, 'entropy')
+        feature_info_gain_gini = comp_feature_information_gain(df_discretized, target_name, f, 'gini')
+        info_gain_entropy.append(feature_info_gain_entropy)
+        info_gain_gini.append(feature_info_gain_gini)
+
+    IV = calculate_woe_iv(df_discretized, ffeatures, target_name)
+
+    if ds in ['spect', 'Zoo']:
+        results_df['Chi2'] = selector_chi_anova.scores_
+    else:
+        results_df['ANOVA'] = selector_chi_anova.scores_
+
+    results_df['embedded_logit'] = lasso_selector.estimator.coef_[0]
+    results_df['embedded_xgboost'] = tree_selector.estimator.feature_importances_
+    results_df['Information_gain_entropy'] = pd.Series(info_gain_entropy)
+    results_df['Information_gain_gini'] = pd.Series(info_gain_gini)
+    # calculate Information Value:
+    results_df['IV'] = pd.Series(IV)
+    results_df['TuRF'] = trelief_selector.feature_importances_
+    std_scaler = MinMaxScaler().fit(results_df)
+    results_df = pd.DataFrame(std_scaler.transform(results_df), columns=results_df.columns)
+    # get the mean score of each criteria
+    results_df_mean = results_df[[x for x in results_df.columns.values]].mean()
+    results_df = results_df.append(results_df_mean, ignore_index=True)
+    results_df['feature'] = ffeatures + ['mean_threshold']
+    # rearrange columns to bring the 'feature' column to the front
+    cols = results_df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    results_df = results_df[cols]
+
+    # the threshold in the mean of means
+    Threshold_min = results_df_mean.min()
+    Threshold_max = results_df_mean.max()
+    results_df.to_csv(os.path.join(out_dir, 'features_scores_%s.csv' % (file_name)), sep=';')
+    with open(os.path.join(out_dir, 'features_scores_%s.csv' % (file_name)), 'a') as fout:
+        fout.write('\n')
+        fout.write('Threshold_min;%s\n' % (Threshold_min))
+        fout.write('Threshold_max;%s\n' % (Threshold_max))
+
+
---
+++ preprocessing/datasets_preprocessing.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import os
+from utils.retrieval import retrieve_artefact
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+
+
+def preprocessing(ds_name, cols, target_name, filename, file_end, target_encoding=None):
+    print(ds_name)
+    if file_end in ['.csv', '.data']:
+        df = retrieve_artefact(filename, file_end, ',')
+    else:
+        df = retrieve_artefact(filename, file_end)
+    df.columns = cols
+    # drop duplicate rows if they exceed 5% of the total df length
+    if ((len(df) - len(df.drop_duplicates())) / len(df)) > 0.05:
+        df = df.drop_duplicates()
+    if ds_name == 'BreastC':  # this dataset has 16 entries with '?' sign indicating missing values
+        df.replace({'?': -1}, inplace=True)
+        df = df.apply(pd.to_numeric)
+        # df['BareNuclei'] = pd.to_numeric(df['BareNuclei'])
+    if ds_name in ['Ionosphere', 'Vehicle']:
+        lbl = LabelEncoder()
+        df[target_name] = lbl.fit_transform(df[target_name])
+    if ds_name == 'Scene':
+        target_cols = ["Beach", "Sunset", "FallFoliage", "Field", "Mountain", "Urban"]
+        df[target_name] = df[target_cols].sum(axis=1)
+        df = df.drop(target_cols, axis=1)
+    if target_encoding:
+        df[target_name].replace(target_encoding, inplace=True)
+        # print(df[target_name].value_counts())
+    if ds_name == 'Diabetic':
+        df[target_name] = pd.to_numeric(df[target_name])
+    objcols = list(df.select_dtypes(exclude=['float64', 'float32', 'int64', 'int32']).columns)
+    lbl = LabelEncoder()
+    for i in objcols:
+        df[i] = lbl.fit_transform(df[i])
+    y = df[target_name]
+    X = df.drop([target_name], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test, df.columns
+
+
+def scale_update(old_dict):
+    scale_df = pd.DataFrame()
+    for x in old_dict.keys():
+      vals = old_dict[x]
+      scale_df[x] = vals.values()
+    df_scaler = MinMaxScaler()
+    scale_df = pd.DataFrame(df_scaler.fit_transform(scale_df), columns=scale_df.columns.values)
+    for i in range(len(old_dict.keys())):
+      keys = list(list(old_dict.values())[i].keys())
+      scaled_vals = list(scale_df[scale_df.columns[i]].values)
+      unscaled_dict = list(old_dict.values())[i]
+      dictionary = dict(zip(keys,scaled_vals))
+      unscaled_dict = unscaled_dict.update(dictionary)
+    return
+
+
+def Discretize(df, filename):
+    for i in df.columns[:-1]:
+        if (len(df[i].value_counts()) >= 10):
+            bins = 4
+        else:
+            bins = len(df[i].value_counts())
+        df[i] = pd.cut(df[i], bins=bins)  # discretize
+        print(df.dtypes)
+        # transform the resulting discretized
+        objcols = list(df.select_dtypes(include=['category']).columns)
+        lbl = LabelEncoder()
+        for i in objcols:
+            df[i] = lbl.fit_transform(df[i])
+    print(len(list(df.select_dtypes(include=['category']).columns)))
+    return df.to_csv('discretized_dataset_%s.csv' % (filename), sep=',', index=False, header=False)
+
+
+# a function to concatenate all the .dat vehicles_files
+def conc_dfs(datasets_folder,Vehicle_files, Vehicle_header):
+    df_total = pd.DataFrame()
+    for vf in Vehicle_files:
+        df = pd.read_csv(vf, sep='\\s', engine='python', header=None)
+        df_total = pd.concat([df_total,df])
+    return df_total.to_csv(os.path.join(datasets_folder,'Vehicles.csv'), header=Vehicle_header, sep=',', index=None)
+
+
+def compute_class_counts(folder, datasets, info_df,out_dir):
+    for ds in datasets:
+        file_name = info_df.loc[info_df['Dataset_name'] == ds, 'files_names'].values[0]
+        target_name = info_df.loc[info_df['Dataset_name'] == ds, 'targets_names'].values[0]
+        df = retrieve_artefact(folder, 'preprocessed_not_discretized_dataset_%s' %(file_name),                               '.csv', ',')
+        with open(os.path.join(out_dir, 'class_counts.txt'), 'a') as fout:
+              fout.write('Dataset: %s' %(ds))
+              fout.write('\n')
+              fout.write('Dataset shape: %s' %(str(df.shape)))
+              fout.write('\n')
+              fout.write(str(df[target_name].value_counts().to_numpy()))
+              fout.write('\n')
