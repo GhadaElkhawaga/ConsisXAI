e4bc694398ed745b5776f78e08558b476f46da45
GhadaElkhawaga <48477434+GhadaElkhawaga@users.noreply.github.com>
2022-10-17 16:29:30 +0200

    Update Consistency_ratios.py

--- evaluation/Consistency_ratios.py
+++ evaluation/Consistency_ratios.py
@@ -24,65 +24,70 @@ def get_threshold(threshold_min, threshold_max):
     threshold = abs(threshold_min - l[idx])
     return threshold, len(l)
 
-
+####################################################################################################################################
 def compute_reds(params_df, threshold):
-    reds = {}
-    reds_weights = {}
-    sorting_criteria = params_df.columns[2:]
-    for sc in sorting_criteria:
-        selected_feats = params_df.iloc[
-            params_df.index[params_df[sc] >= threshold], params_df.columns.get_loc("feature")]
-        selected_weights = params_df.iloc[params_df.index[params_df[sc] >= threshold],
-                                          params_df.columns.get_loc(sc)]
-        reds[sc] = selected_feats.index.values.tolist()
-        reds_weights[sc] = selected_weights.tolist()
-    return reds, reds_weights
+  reds = {}
+  reds_weights = {}
+  sorting_criteria = params_df.columns[2:]
 
+  for sc in sorting_criteria:       
+    selected_feats = params_df.iloc[params_df.index[params_df[sc] >= threshold], params_df.columns.get_loc("feature")]
+    selected_weights = params_df.iloc[params_df.index[params_df[sc] >= threshold], params_df.columns.get_loc(sc)]
+    reds[sc] = selected_feats.index.values.tolist()
+    reds_weights[sc] = selected_weights.tolist()
+  return reds, reds_weights
 
+#########################################################################################################################################
 def compute_core(reds):
-    return list(reduce(set.intersection, (set(val) for val in reds.values())))
+   return list(reduce(set.intersection, (set(val) for val in reds.values())))
+#####################################################################################################################################
+def compute_reducts_core(file_name,ds):
+      params_df = retrieve_artefact(logs_dir, 'features_scores_%s' %(file_name), '.csv', ';')
+      Threshold_min = ast.literal_eval(params_df.iloc[-2,1])
+      Threshold_max = ast.literal_eval(params_df.iloc[-1,1])
+         
+      params_df = params_df.drop(params_df.index[params_df['feature']=='mean_threshold'])
+      Threshold = Threshold_min
+      reds, reds_weights = compute_reds(params_df, Threshold)
+      core = compute_core(reds)     
 
-
-def compute_reducts_core(out_dir, file_name, reds_core_dir):
-    params_df = retrieve_artefact(out_dir, 'features_scores_%s' %(file_name), '.csv', ';')
-    threshold_min = ast.literal_eval(params_df.iloc[-2, 1])
-    threshold_max = ast.literal_eval(params_df.iloc[-1, 1])
-    params_df = params_df.drop(params_df.index[params_df['feature'] == 'mean_threshold'])
-    threshold = threshold_min
-    reds, reds_weights = compute_reds(params_df, threshold)
-    core = compute_core(reds)
-    i, length = 0, 1
-    while (len(core) == 0) and (i < length):
-        threshold, length = get_threshold(threshold_min, threshold_max)
-        reds, reds_weights = compute_reds(params_df, threshold)
-        core = compute_core(reds)
+      i, length = 0 ,1
+      while (len(core) == 0) and (i < length):
+        Threshold, length = get_threshold(Threshold_min, Threshold_max)
+        reds, reds_weights = compute_reds(params_df, Threshold)
+        core = compute_core(reds)       
         i += 1
-    # to calculate the weight of each feature in the core
-    # taking the average of all weights of the same feature in different reducts
-    core_feat_weights = []
-    for x in core:
+      print(reds)
+      print(core)
+        
+      #to calculate the weight of each feature in the core
+      #taking the average of all weights of the same feature in different reducts
+      core_feat_weights = []
+      for x in core:
         red_feat_weights = []
         for key, val in reds.items():
-            idx = val.index(x)
-            red_feat_weights.append(reds_weights[key][idx])
-        core_feat_weights.append(sum(red_feat_weights) / len(red_feat_weights))
-    with open(os.path.join(reds_core_dir, 'reds_%s_python.json' %(file_name)), 'w') as outfile:
+          idx = val.index(x)
+          red_feat_weights.append(reds_weights[key][idx])
+        core_feat_weights.append(sum(red_feat_weights)/len(red_feat_weights))
+      
+      with open(os.path.join(logs_dir, 'reds_%s_python.json'%(file_name)), 'w') as outfile:
         json.dump(reds, outfile)
-    with open(os.path.join(reds_core_dir, 'core_%s_python.json' %(file_name)), 'w') as outfile:
+      with open(os.path.join(logs_dir, 'core_%s_python.json'%(file_name)), 'w') as outfile:
         json.dump(core, outfile)
 
 
-def ComputeRatio(selectedsetByXYZ, originalset, flag=None):
-    selected_feats = selectedsetByXYZ.keys()
-    sum_numenator_set = 0
-    original_set_sum = len(originalset) #each item in the reductset is assigned '1'
-    numenator_set = set(originalset).intersection(set(selected_feats))
-    intesection_vol = len(numenator_set)
-    for x in numenator_set:
-        sum_numenator_set += selectedsetByXYZ[x]
-    if flag == 'regular':
-        return sum_numenator_set/original_set_sum , sum_numenator_set, intesection_vol
-    elif flag == 'experimental':
-        return intesection_vol/original_set_sum, intesection_vol
-    else:
-        return
+#compute Reduct/Core ratios
+def ComputeRatio(selectedsetByXYZ,originalset, flag=None):
+  selected_feats = selectedsetByXYZ.keys() 
+  sum_numenator_set = 0
+  original_set_sum = len(originalset) #each item in the reductset is assigned '1'
+  numenator_set = set(originalset).intersection(set(selected_feats))
+  intesection_vol = len(numenator_set)
+  for x in numenator_set:
+    sum_numenator_set += selectedsetByXYZ[x] 
+  if flag == 'regular':
+    return sum_numenator_set/original_set_sum , sum_numenator_set, intesection_vol
+  elif flag == 'experimental':
+    return intesection_vol/original_set_sum, intesection_vol
+  else:
+    return
